ef39f3a8f4164c22b1e81abd894f995484b7399b
Chen, Hao <darkray@126.com>
2021-11-24 10:56:24 -0500

    [feat]Added screen quad
    Signed-off-by: Chen, Hao <darkray@126.com>

---
+++ screen_quad_helper.cpp
@@ -0,0 +1,31 @@
+#include "screen_quad_helper.hpp"
+
+#include <glm/glm.hpp>
+
+ScreenQuad::ScreenQuad() {
+    // create vertices & texcoords for screen-sized quad
+    const float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+    // screen quad VAO
+    GLuint quadVBO;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    // unbind buffer
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
